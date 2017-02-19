5d44aee9e889096a3ac4878a505b1ab38031b7d4
zhang-yongjiang <zhangyongjiang47@gmail.com>
2017-02-19 19:31:30 +0800

    First commit.

---
+++ app/src/main/java/app/gourd/com/gourd/GourdActivity.java
@@ -0,0 +1,13 @@
+package app.gourd.com.gourd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GourdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gourd);
+    }
+}
